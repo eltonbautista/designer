4b2f7f6a93800b4ce4ba80e216952f83c9b83cdb
Elton Bautista <eltonbestaha@gmail.com>
2022-09-12 12:28:46 -0600

    Added SEO Optimization features: sitemap, robots.txt, keywords, meta, and more

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,23 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import robotsTxt from "astro-robots-txt";
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  site: "https://www.eltonbautista.com/",
+  experimental: {
+    integrations: true,
+  },
+  integrations: [
+    tailwind(),
+    react(),
+    robotsTxt({
+      sitemap: false,
+    }),
+    sitemap(),
+  ],
 });
