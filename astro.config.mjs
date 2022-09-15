1500e1b0678c3cceb4926c2421d0ba60e0b0b1ad
Elton Bautista <eltonbestaha@gmail.com>
2022-09-15 09:40:16 -0600

    Optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import robotsTxt from "astro-robots-txt";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
-
 import sitemap from "@astrojs/sitemap";
+import { astroImageTools } from "astro-imagetools";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
@@ -19,5 +20,7 @@ export default defineConfig({
       sitemap: false,
     }),
     sitemap(),
+    compress(),
+    astroImageTools,
   ],
 });
